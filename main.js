c432c3ae6b84bdafc21a18fa4ff603d2bcebd642
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-01-25 15:29:52 +0100

    inserito axios

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 const app = new Vue({
     el: '#app',
+    data:{
+        email: [
+
+        ]
+    },
+    mounted: function() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+        .catch(function (error) {
+        // handle error
+        console.log(error);
+  })
+
+    },
 })
